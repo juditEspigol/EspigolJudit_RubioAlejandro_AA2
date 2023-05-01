f573641e9b31634b60b7475c295b42694ef1180c
Judit Espigol <judith.espigol@enti.cat>
2023-05-01 17:44:42 +0200

    Thinking InitGame and Print

--- AA2_TechProgra/AA2_TechProgra/AA2_TechProgra.cpp
+++ AA2_TechProgra/AA2_TechProgra/AA2_TechProgra.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <list>
 
 int main()
 {
+	/*
+	// Create the dynamic array 
+	char** actualRoom;
 
+
+	// Create a list of all our rooms 
+	std::list<Room> rooms;
+	for (int i = 0; i < static_cast<int>(TypeRoom::COUNT); ++i)
+	{
+		Room newRoom(static_cast<TypeRoom>(i), 5 * (i + 1), 5 * (i + 1));
+		rooms.push_back(newRoom);
+	}
+	std::list<Room>::iterator itRooms = rooms.begin();
+
+	// Init our player 
+	Player player1(3, 3);
+
+
+	actualRoom = itRooms->InicializeRoom(player1);
+
+	itRooms->PrintRoom(actualRoom);
+	*/
 }
--- AA2_TechProgra/AA2_TechProgra/Room.cpp
+++ AA2_TechProgra/AA2_TechProgra/Room.cpp
@@ -35,3 +35,60 @@ Room::~Room()
 	myRoom = nullptr;
 }
 
+
+/*
+// Constructor
+Room::Room(TypeRoom typeRoom, int numRows, int numColums)
+{
+	m_typeRoom = typeRoom;
+	m_numRows = numRows;
+	m_numColums = numColums;
+	m_prevDoor = numColums / 2;
+	m_nextDoor = numColums / 2;
+}
+
+// Methods
+char** Room::InicializeRoom(const Player& p1)
+{
+	// Create the 2D dynamic array
+	char** myRoom = new char* [m_numRows];
+	for (int y = 0; y < m_numRows; ++y)
+	{
+		myRoom[y] = new char[m_numColums];
+		for (int x = 0; x < m_numColums; ++x)
+		{
+			if (y == p1.m_posY && x == p1.m_posX)
+			{
+				myRoom[y][x] = '^';
+			}
+			else if (y == 0 && x == m_nextDoor)
+			{
+				myRoom[y][x] = CHAR_DOOR;
+			}
+			else if (y == 0 || y == m_numRows - 1 || x == 0 || x == m_numColums - 1)
+			{
+				myRoom[y][x] = CHAR_WALL;
+			}
+			else
+			{
+				myRoom[y][x] = CHAR_EMPTY;
+			}
+		}
+	}
+	return myRoom;
+}
+
+void Room::PrintRoom(char** myRoom) const
+{
+	for (int y = 0; y < m_numRows; ++y)
+	{
+		std::cout << std::endl << " ";
+		for (int x = 0; x < m_numColums; ++x)
+		{
+			std::cout << myRoom[y][x] << " ";
+		}
+	}
+	std::cout << std::endl;
+}
+
+*/
