4de1502bdb6133dc202f7a8dab75ee43019de9eb
AlexRubio14 <alejandro.rubio@enti.cat>
2023-05-03 02:19:56 +0200

    It only read the first row of the txt and get out the loop

--- AA2_TechProgra/AA2_TechProgra/AA2_TechProgra.cpp
+++ AA2_TechProgra/AA2_TechProgra/AA2_TechProgra.cpp
@@ -7,15 +7,35 @@ int main()
 
     Player p1; 
 
+    std::ifstream config("config.txt");
+
     // Create a list of all our rooms 
     std::list<Room> rooms;
     for (int i = 0; i < static_cast<int>(TypeOfRoom::COUNT); ++i)
     {
+        Size size[static_cast<int>(TypeOfRoom::COUNT)];
         int width, height;
+        char basura;
 
-        Room newRoom(static_cast<TypeOfRoom>(i), 5 * (i + 1), 5 * (i + 1));
-        rooms.push_back(newRoom);
+        if (config.is_open())
+        {
+            int j = 0;
+            while (config >> width >> basura >> height && j < 3)
+            {
+                size[j].width = width;
+                size[j].height = height;
+                j++;
+            }
+            Room newRoom(static_cast<TypeOfRoom>(i), size[i].width, size[i].height);
+            rooms.push_back(newRoom);
+        }
+        else
+        {
+            std::cout << " An error has occurred trying to open the file";
+        }
     }
+
+    config.close();
     std::list<Room>::iterator actualRoomIt = rooms.begin();
 
     myRoom = actualRoomIt->CreateRoom(); 
--- AA2_TechProgra/AA2_TechProgra/Game.h
+++ AA2_TechProgra/AA2_TechProgra/Game.h
@@ -1,4 +1,10 @@
 #pragma once
 #include "Includes.h"
 
+struct Size
+{
+	int width;
+	int height;
+};
+
 void exitGame(bool& isPlaying);
